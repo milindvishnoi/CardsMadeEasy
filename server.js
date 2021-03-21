86e0a290004c588539131bbfb824a7def74ec92c
mills <milindvishnoi@gmail.com>
2021-03-22 05:18:06 +0530

    partial working heruko deploy

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+	// sending a string
+	//res.send('This should be the root route!')
+
+	//sending some HTML
+	res.sendFile(path.join(__dirname, './example.html'))
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`)
-}) 
+})
